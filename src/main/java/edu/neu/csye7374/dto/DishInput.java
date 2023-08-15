b8cbf7c46826ff7881d5e40062aff2512a114e0b
Nishaanth-44 <114043295+Nishaanth-44@users.noreply.github.com>
2023-08-15 00:11:38 -0400

    Added BaseRepository and DishInput

---
+++ src/main/java/edu/neu/csye7374/dto/DishInput.java
@@ -0,0 +1,34 @@
+package edu.neu.csye7374.dto;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@lombok.Data
+@NoArgsConstructor
+public class DishInput {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean isVegan;
+    private boolean isGlutenFree;
+    private int preparationTime;
+    private int calories;
+    private ReturnType.DishType dishType;
+
+    private int quantity;
+    private boolean isCustomized;
+
+
+
+    private int cheesePreference;
+
+    private String extraMeat;
+
+    private List<String> toppings;
+}
